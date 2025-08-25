f5e4f304bdf4c2148a34b960b083f9bc8858036a
kiranR007 <kirankiru363@gmail.com>
2025-08-25 18:19:42 +0530

    designation change

---
+++ src/app/service/master.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiResponseModel } from '../models/class/interface/role';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MasterService {
+
+  constructor(private http:HttpClient) { }
+
+  getDesignation():Observable<ApiResponseModel> {
+    return this.http.get<ApiResponseModel>("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllDesignation")
+  }
+}
